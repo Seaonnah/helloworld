01f65c697b901032bc69498a2e77b7d2606e1fb2
Seaon Shin <seaonnah@seaons-macbook.local>
2018-09-23 13:02:48 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Seaon Shin on 9/23/18.
+//  Copyright © 2018 Seaon Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
