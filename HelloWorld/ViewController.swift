91c2538151d5af98f6f3ff6ebc3fdc99a349adf9
Seaon Shin <seaonnah@seaons-macbook.lan>
2018-10-01 20:38:37 -0700

    added button to change color

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func didTapButton(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.white
+    }
+    
 
 }
-
